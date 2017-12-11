0877d6cb654e2736de585090f1e127699a93732f
Playinteractive <admin@playinteractive.com>
2017-12-11 17:57:19 +0100

    Update class.mysqli.php

--- class.mysqli.php
+++ class.mysqli.php
@@ -2,26 +2,29 @@
 
 class mysqli_link
 {
-	public $mysqli = NULL;
+	private $mysqli = NULL;
 
-	public function __construct($username, $password, $database, $offset = '+00:00', $charset = 'utf8mb4', $host = 'localhost')
+    private $options = ['host' => 'localhost', 'port' => 3306, 'persistent' => FALSE, 'charset' => 'utf8mb4', 'offset' => '+00:00'];
+
+	function __construct($username, $password, $database, array $options)
 	{
-		$this->mysqli = new mysqli($host, $username, $password, $database);
-		$this->mysqli->set_charset($charset);
-		$this->mysqli->query("SET time_zone = '$offset'");
+		$this->options = array_merge($this->options, $options);
+		$this->mysqli = new mysqli($this->options['persistent'] ? 'p:' . $this->options['host'] : $this->options['host'], $username, $password, $database, $this->options['port']);
+		$this->mysqli->set_charset($this->options['charset']);
+		$this->mysqli->query("SET time_zone = '{$this->options['offset']}'");
 	}
 
-	public function insert_id()
+	function insert_id()
 	{
 		return $this->mysqli->insert_id;
 	}
 
-	public function query($query)
+	function query($query)
 	{
 		return $this->mysqli->query($query);
 	}
 
-	public function real_escape_string($str)
+	function real_escape_string($str)
 	{
 		return $this->mysqli->real_escape_string($str);
 	}
