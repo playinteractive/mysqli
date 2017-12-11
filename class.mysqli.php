e12eefbeff142cea80b9a0a76775612f44b9cd23
Playinteractive <admin@playinteractive.com>
2017-12-11 18:02:35 +0100

    Update class.mysqli.php

--- class.mysqli.php
+++ class.mysqli.php
@@ -4,7 +4,7 @@ class mysqli_link
 {
 	private $mysqli = NULL;
 
-    private $options = ['host' => 'localhost', 'port' => 3306, 'persistent' => FALSE, 'charset' => 'utf8mb4', 'offset' => '+00:00'];
+	private $options = ['host' => 'localhost', 'port' => 3306, 'persistent' => FALSE, 'charset' => 'utf8mb4', 'offset' => '+00:00'];
 
 	function __construct($username, $password, $database, array $options)
 	{
