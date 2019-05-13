b1d41c3ec4f7a72f7a3146b5867448063ddffc35
Playinteractive <github@playinteractive.com>
2019-05-13 05:05:36 +0200

    Update mysqli.class.php

--- mysqli.class.php
+++ mysqli.class.php
@@ -1,23 +1,22 @@
 <?php
-
 /**
  * @author    Playinteractive <mysqli.github@playinteractive.com>
  * @link      http://www.playinteractive.com
  * @copyright 2017 Playinteractive
  */
 
-class mysqli_connect
-{
+class mysqli_connect {
+	
 	public $mysqli = NULL;
 
 	private $options = ['host' => 'localhost', 'port' => 3306, 'persistent' => FALSE, 'charset' => 'utf8mb4', 'offset' => '+00:00'];
 
-	function __construct($username, $password, $database, array $options = array())
-	{
-		$this->options = array_merge($this->options, $options);
-		$this->mysqli = new mysqli($this->options['persistent'] ? 'p:' . $this->options['host'] : $this->options['host'], $username, $password, $database, $this->options['port']);
-		$this->mysqli->set_charset($this->options['charset']);
-		$this->mysqli->query("SET time_zone = '{$this->options['offset']}'");
+	function __construct($username, $password, $database, array $options = array())	{
+		
+	$this->options = array_merge($this->options, $options);
+	$this->mysqli = new mysqli($this->options['persistent'] ? 'p:' . $this->options['host'] : $this->options['host'], $username, $password, $database, $this->options['port']);
+	$this->mysqli->set_charset($this->options['charset']);
+	$this->mysqli->query("SET time_zone = '{$this->options['offset']}'");
 	}
 
 	function insert_id()
