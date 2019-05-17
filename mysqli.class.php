a2002e9f6408395c3376516b992caf23b96a1698
Playinteractive <github@playinteractive.com>
2019-05-17 20:27:34 +0200

    Update mysqli.class.php

--- mysqli.class.php
+++ mysqli.class.php
@@ -3,34 +3,36 @@
  * @author    Playinteractive <mysqli.github@playinteractive.com>
  * @link      https://www.playinteractive.com
  * @copyright 2017 Playinteractive
+ * @source    https://github.com/playinteractive/mysqli
  */
 
-class mysqli_connect {
-	
-	public $mysqli = NULL;
+class mysqli_connect
+{
+    public $mysqli = NULL;
 
-	private $options = ['host' => 'localhost', 'port' => 3306, 'persistent' => FALSE, 'charset' => 'utf8mb4', 'offset' => '+00:00'];
+    private $options = ['host' => 'localhost', 'port' => 3306, 'persistent' => FALSE, 'charset' => 'utf8mb4', 'offset' => '+00:00'];
 
-	function __construct($username, $password, $database, array $options = array())	{
+    function __construct($username, $password, $database, array $options = array())
+    {
 		
-	$this->options = array_merge($this->options, $options);
-	$this->mysqli = new mysqli($this->options['persistent'] ? 'p:' . $this->options['host'] : $this->options['host'], $username, $password, $database, $this->options['port']);
-	$this->mysqli->set_charset($this->options['charset']);
-	$this->mysqli->query("SET time_zone = '{$this->options['offset']}'");
-	}
+    $this->options = array_merge($this->options, $options);
+    $this->mysqli = new mysqli($this->options['persistent'] ? 'p:' . $this->options['host'] : $this->options['host'], $username, $password, $database, $this->options['port']);
+    $this->mysqli->set_charset($this->options['charset']);
+    $this->mysqli->query("SET time_zone = '{$this->options['offset']}'");
+    }
 
-	function insert_id()
-	{
-		return $this->mysqli->insert_id;
-	}
+    function insert_id()
+    {
+        return $this->mysqli->insert_id;
+    }
 
-	function query($query)
-	{
-		return $this->mysqli->query($query);
-	}
+    function query($query)
+    {
+        return $this->mysqli->query($query);
+    }
 
-	function real_escape_string($str)
-	{
-		return $this->mysqli->real_escape_string($str);
-	}
+    function real_escape_string($str)
+    {
+        return $this->mysqli->real_escape_string($str);
+    }
 }
